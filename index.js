46d06954654247353a4be33e272ef10c5cae15ba
Samuca23 <samuelchiodini@hotmail.com>
2022-11-23 20:53:01 -0300

    up 1

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 class Speak {
-    
+
+    constructor() {
+        this.createLayout();
+    }
+
+    createLayout = () => {
+        let oDivContainer = document.getElementById('container');
+
+    }
+
+    /**
+     * Função para criar o Elemento de TextArea
+     * 
+     * @param {string} sContent 
+     * @param {string} sClass 
+     * @param {string} sId 
+     * @returns 
+     */
+    createTextArea = (sContent = '', sId = '', sClass = '') => {
+        let oTextArea = document.createElement('textarea');
+        oTextArea.setAttribute('id', `inputDescricao${sId}`);
+        oTextArea.setAttribute('class', sClass);
+        oTextArea.value = sContent;
+        oTextArea.addEventListener('change', () => {
+            updateCard(sId)
+        })
+
+        return oTextArea;
+    }
+
+    /**
+     * Função para criar div.
+     * 
+     * @param {string} sId 
+     * @param {string} sClass 
+     * @returns 
+     */
+    createDiv = (sId, sClass) => {
+        let oDiv = document.createElement('div');
+        oDiv.setAttribute('id', `div${sId}`);
+        oDiv.setAttribute('class', sClass);
+
+        return oDiv;
+    }
 }
